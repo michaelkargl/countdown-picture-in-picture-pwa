6d73df127c57d268e940ca2b697275a5354e76bb
Michael Kargl <michael.kargl@zuehlke.com>
2025-04-26 13:13:02 +0200

    feat: copies styling when opening pip

--- src/components/PictureInPictureContainer/PictureInPictureContainer.tsx
+++ src/components/PictureInPictureContainer/PictureInPictureContainer.tsx
@@ -1,30 +1,34 @@
 import * as React from "react"
-import { PropsWithChildren, useMemo, useState } from "react"
+import { PropsWithChildren, ReactElement, useMemo, useState } from "react"
 import { WindowExtended } from "../../models/WindowExtended"
 
-type PictureInPictureClosedCallback = () => void;
+type PictureInPictureClosedCallback = () => void
 type PictureInPictureContainerProps = PropsWithChildren<{
   id: string
-}>;
+}>
 
-export const PictureInPictureContainer: React.FC<PictureInPictureContainerProps> = (props: PictureInPictureContainerProps) => {
-  const pipContainerId = useMemo(() => getPipContainerId(props.id), [props.id]);
-  const pipButtonId = useMemo(() => getPipButtonId(props.id), [props.id]);
+export const PictureInPictureContainer: React.FC<PictureInPictureContainerProps> = (
+  props: PictureInPictureContainerProps
+): React.ReactElement => {
+  const pipContainerId = useMemo(() => getPipContainerId(props.id), [props.id])
+  const pipButtonId = useMemo(() => getPipButtonId(props.id), [props.id])
 
-  return (<div id={pipContainerId}>
-    <div id={pipButtonId} onClick={() => showPictureInPictureAsync(props.id)}>
-      {props.children}
+  return (
+    <div id={pipContainerId}>
+      <div id={pipButtonId} onClick={() => showPictureInPictureAsync(props.id)}>
+        {props.children}
+      </div>
     </div>
-  </div>)
+  )
 }
 
 function getPipContainerId(id: string): string {
-  console.count(`getPipContainerId ${id}`);
+  console.count(`getPipContainerId ${id}`)
   return `pip-container-${id}`
 }
 
 function getPipButtonId(id: string): string {
-  console.count(`getPipButtonId ${id}`);
+  console.count(`getPipButtonId ${id}`)
   return `pip-button-${id}`
 }
 
@@ -33,46 +37,65 @@ function onPictureInPictureClose(callback: PictureInPictureClosedCallback) {
     throw new Error("The current browser does not support Picture in Picture")
   }
 
-  const pipWindow = window.documentPictureInPicture.window;
-  pipWindow. addEventListener("pagehide", (event) => {
-    callback();
-  });
-}
-
-
-function togglePipButtonVisibility(pipId: string) {
-  const pipButtonId = getPipButtonId(pipId);
-  const pipButton = document.querySelector(`#${pipButtonId}`)
-  if (!pipButton) {
-    throw new Error(`Unable to set visibility of element [#${pipButtonId}]: it cound not be found.`)
+  const pipWindow = window.documentPictureInPicture.window
+  if (!pipWindow) {
+    console.debug("No Picture in Picture window found / none seems to be open")
+    return
   }
 
-  const visibility = pipButton.checkVisibility();
-  const newVisibility = visibility ? "hidden" : "visible";
-  pipButton.setAttribute("visibility", newVisibility);
+  pipWindow.addEventListener("pagehide", event => {
+    callback()
+  })
 }
 
-async function showPictureInPictureAsync(pipId: string): Promise<void> {
+function togglePipButtonVisibility(pipId: string) {
+  const pipButtonId = getPipButtonId(pipId)
+  const pipButton = document.querySelector(`#${pipButtonId}`)
+  if (!pipButton) {
+    throw new Error(
+      `Unable to set visibility of element [#${pipButtonId}]: it cound not be found.`
+    )
+  }
 
+  const visibility = pipButton.checkVisibility()
+  const newVisibility = visibility ? "hidden" : "visible"
+  pipButton.setAttribute("visibility", newVisibility)
+}
+
+/**
+ * @see https://developer.chrome.com/docs/web-platform/document-picture-in-picture/
+ * @see https://hackernoon.com/how-to-document-picture-in-picture-in-react-with-typescript
+ */
+async function showPictureInPictureAsync(pipId: string): Promise<void> {
   if (!supportsPictureInPicture(window)) {
     throw new Error("The current browser does not support Picture in Picture")
   }
 
-  const pipContainerId = getPipContainerId(pipId);
+  const pipContainerId = getPipContainerId(pipId)
   const pipContainer = document.querySelector(`#${pipContainerId}`)
   if (!pipContainer) {
-    throw new Error(`Unable to set element with id [${pipContainerId}] into Picture in Picture mode: it is null.`)
+    throw new Error(
+      `Unable to set element with id [${pipContainerId}] into Picture in Picture mode: it is null.`
+    )
   }
 
-  const pipDocument = pipContainer.firstElementChild;
+  const pipDocument = pipContainer.firstChild
+  if (!pipDocument) {
+    console.error(
+      `Unable to show picture in picture. Element [#${pipContainerId}] has no children to view.`
+    )
+    return
+  }
+
   const pipWindow = await window.documentPictureInPicture.requestWindow({
     disallowReturnToOpener: true
-  });
-  pipWindow.document.body.append(pipDocument);
+  })
+  copyStylesTo(document.documentElement, pipWindow.document.documentElement)
+  pipWindow.document.body.append(pipDocument)
 
   onPictureInPictureClose(() => {
-    pipContainer.append(pipDocument);
-    togglePipButtonVisibility(pipId);
+    pipContainer.append(pipDocument)
+    togglePipButtonVisibility(pipId)
   })
 
   if (!supportsPictureInPicture(window)) {
@@ -81,17 +104,61 @@ async function showPictureInPictureAsync(pipId: string): Promise<void> {
 }
 
 function supportsPictureInPicture(win: object): win is WindowExtended {
-  const extWindow = win as any as WindowExtended
+  const extWindow = (win as any) as WindowExtended
   return !!extWindow && !!extWindow.documentPictureInPicture
 }
 
-/**
- *
- * @param id the id of the html element to set the picture in picture state
- * @param state true = enter picture in picture, false = exit picture in picture
- * @see https://developer.chrome.com/docs/web-platform/document-picture-in-picture/
- * @see https://hackernoon.com/how-to-document-picture-in-picture-in-react-with-typescript
- */
-async function setPictureInPictureAsync(id: string, state: boolean): Promise<void> {
+function* getStyleSheets(): IterableIterator<CSSStyleSheet> {
+  const styleSheets = document.styleSheets
+  for (let i = 0; i < styleSheets.length; i++) {
+    yield styleSheets[i]
+  }
+}
 
+function* getCSSRules(styleSheet: CSSStyleSheet): IterableIterator<CSSRule> {
+  const cssRules = styleSheet.cssRules
+  if (!cssRules) {
+    console.debug("Stylesheet does not contain any CSS rules. Skipping...")
+    return
+  }
+
+  for (let i = 0; i < cssRules.length; i++) {
+    yield cssRules[i]
+  }
+}
+
+function getCSSRulesAsArray(styleSheet: CSSStyleSheet): CSSRule[] {
+  const rules: CSSRule[] = []
+  for (const rule of getCSSRules(styleSheet)) {
+    rules.push(rule)
+  }
+  return rules
+}
+
+function copyStylesTo(source: HTMLElement, destination: HTMLElement) {
+  for (let sheet of getStyleSheets()) {
+    if (!sheet.cssRules) {
+      continue
+    }
+
+    const cssRuleArray = getCSSRulesAsArray(sheet)
+    if (!cssRuleArray?.length) {
+      continue
+    }
+
+    try {
+      const cssRules = cssRuleArray.map(rule => rule.cssText).join("")
+      const style = document.createElement("style")
+      style.textContent = cssRules
+      destination.ownerDocument.head.appendChild(style)
+
+    } catch (ex) {
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.type = sheet.type;
+      link.media = sheet.media.mediaText;
+      link.href = link.href;
+      destination.ownerDocument.head.appendChild(link)
+    }
+  }
 }
