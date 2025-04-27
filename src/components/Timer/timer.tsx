544df4b4c79c3933ba93e15c8cdb03d4a91aef75
Michael Kargl <michael.kargl@zuehlke.com>
2025-04-27 07:27:53 +0200

    feat: creates functional progress timer bar

--- src/components/Timer/timer.tsx
+++ src/components/Timer/timer.tsx
@@ -1,56 +1,65 @@
 import * as React from "react"
-import { useEffect, useState } from "react"
 import { TimerEntity } from "../../models"
 import { PictureInPictureContainer } from "../PictureInPictureContainer/PictureInPictureContainer"
 import { ProgressBar } from "../ProgressBar/ProgressBar"
 import "./timer.css"
 import { DateTime } from "luxon"
+import Countdown, { CountdownRenderProps } from "react-countdown"
+import { CountdownRendererFn } from "react-countdown/dist/Countdown"
 
 export type TimerProps = {
   timer: TimerEntity
 }
 
 export const Timer: React.FC<TimerProps> = (props: TimerProps) => {
-  const [now, setNow] = useState(DateTime.now())
-  const percent = getTimerPercentage(now, props.timer.endTime)
+  const renderer: CountdownRendererFn = (
+    rendererProps: CountdownRenderProps
+  ) => {
+    const { hours, minutes, seconds, completed } = rendererProps
 
-  useEffect(() => {
-    console.debug("useEffect: componentWillMount")
-
-    // Elements in this level of userEffect will be called once (mind the empty dependency array [])
-    // https://www.freecodecamp.org/news/react-lifecycle-methods-and-hooks-for-beginners/
-    console.debug(
-      "Initializing a ticker with a [%ims] interval.",
-      props.timer.refreshIntervalInMs
-    )
-    const ticker = setInterval(() => {
-      setNow(() => DateTime.now())
-    }, 1000)
-
-    // The returned function will be called before unmounting the component / cleanup function
-    // https://robertmarshall.dev/blog/componentwillunmount-functional-components-react/
-    return () => {
-      console.debug("useEffect: componentWillUnmount")
-      clearInterval(ticker)
+    if (completed) {
+      return <span>Timer finished 🎇</span>
     }
 
-    // Empty dependency array, runs only once
-  }, [])
+    const percent = getTimerPercentage(props.timer) * 100
+    return (
+      <ProgressBar percentage={percent} color={"green"}>
+        <span>
+          {hours}:{minutes}:{seconds} | {percent}
+        </span>
+      </ProgressBar>
+    )
+  }
 
   return (
     <PictureInPictureContainer id={props.timer.id}>
       <div className="timer">
-        <ProgressBar percentage={percent} color={"green"}>
-          <span>{percent}</span>
-        </ProgressBar>
+        <Countdown
+          date={props.timer.endTime.toJSDate()}
+          intervalDelay={props.timer.refreshIntervalInMs}
+          renderer={renderer}
+          precision={2}
+        >
+          <span>Timer finished</span>
+        </Countdown>
       </div>
     </PictureInPictureContainer>
   )
 }
 
-function getTimerPercentage(start: DateTime, end: DateTime): number {
-  const duration = end.diff(start);
-  const remaining = end.diffNow();
+function toDateTime(date: Date | number | string): DateTime | undefined {
+  if (date instanceof Date) {
+    return DateTime.fromJSDate(date)
+  }
+  if (typeof date === "number") {
+    return DateTime.fromMillis(date)
+  }
+  return DateTime.fromISO(date.toString())
+}
+
+function getTimerPercentage(timer: TimerEntity, precision = 4): number {
+  const duration = timer.endTime.diff(timer.startTime)
+  const remaining = timer.endTime.diffNow()
   const percentage = remaining.milliseconds / duration.milliseconds
-  return percentage * 100
+  return +percentage.toFixed(precision)
 }
